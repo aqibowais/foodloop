16989e793578363f0d3fe03cc9e89a6ef75db94a
Aqib Owais <owaisharoon60@gmail.com>
2026-01-28 03:14:27 +0500

    food listing crud

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'core/theme/app_theme.dart';
 import 'features/onboarding/presentation/screens/splash_screen.dart';
 
